157359422081232528247c97d00bd01e82dcd752
Pradhyumna <pradhyumna.gopisetti2007@gmail.com>
2026-01-03 20:12:06 +0530

    updated the src code

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// -------------------------- CLEAN & STEM WORDS --------------------------
+//  *Cleaning and *stemming words
 string cleanWord(const string &s) {
     string out;
-    for (char c : s) {
-        if (isalnum(c)) out.push_back(tolower(c));
-        else out.push_back(' ');
+    for(char c:s){
+        if(isalnum(c)){
+            out.push_back(tolower(c));
+        }else{
+            out.push_back(' ');
+        }
     }
     return out;
 }
@@ -14,31 +17,40 @@ string cleanWord(const string &s) {
 string stemWord(const string &w) {
     string s = w;
     int n = s.size();
-    if (n <= 3) return s;
-
-    if (n > 4 && s.substr(n - 3) == "ies") return s.substr(0, n - 3) + "y";
-    if (n > 4 && s.substr(n - 2) == "es") return s.substr(0, n - 2);
-    if (s[n - 1] == 's') return s.substr(0, n - 1);
-    if (n > 5 && s.substr(n - 3) == "ing") return s.substr(0, n - 3);
-    if (n > 4 && s.substr(n - 2) == "ed") return s.substr(0, n - 2);
-
+    if(n<=3){
+        return s;
+    }
+    //eg. studies is converted to study
+    if(n>4 && s.substr(n-3) == "ies"){
+        return s.substr(0,n-3)+"y";
+    }
+    //eg. glasses is converted to glass
+    if(n>4 && s.substr(n-2) == "es"){
+        return s.substr(0,n-2);
+    }
+    if(s[n-1] == 's'){
+        return s.substr(0,n-1);
+    }
+    if(n>5 && s.substr(n-3) == "ing"){
+        return s.substr(0,n-3);
+    }
+    if(n>4 && s.substr(n-2) == "ed"){
+        return s.substr(0,n-2);
+    }
     return s;
 }
 
 // -------------------------- PARSING FILES --------------------------
-void parseFiles(const vector<string> &files,
-                unordered_map<string, vector<int>> &termFreq,
-                vector<int> &totalWords) {
+void parseFiles(const vector<string> &files,unordered_map<string, vector<int>> &termFreq,vector<int> &totalWords) {
     int N = files.size();
     totalWords.resize(N, 0);
 
     for (int file = 0; file < N; file++) {
         ifstream in(files[file]);
         if (!in.is_open()) {
-            cerr << "Error opening: " << files[file] << "\n";
+            cout << "Error opening: " << files[file] << endl;
             continue;
         }
-
         string line;
         while (getline(in, line)) {
             string cleaned = cleanWord(line);
@@ -147,11 +159,8 @@ void searchMulti(const vector<string> &words, const vector<string> &files,
 // -------------------------- MAIN --------------------------
 int main() {
     vector<string> files = {
-        "data/f1.txt",
-        "data/f2.txt",
-        "data/f3.txt",
-        "data/f4.txt",
-        "data/f5.txt"
+        "file1.txt","file2.txt","file3.txt","file4.txt",
+        "file5.txt","file6.txt","file7.txt","xyz.txt"
     };
 
     unordered_map<string, vector<int>> termFreq;
